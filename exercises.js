5b74947f6aa7e30b66d960377318c4a433c5ec5c
Felix <felix>
2022-06-22 17:25:24 +1000

    Added objects and classes

--- exercises.js
+++ exercises.js
@@ -17,3 +17,33 @@
 // b) add a method called displayInfo that console.logs the properties
 // c) Using this class, create a new instance (object) with the values of the object we made above
 // d) call this Instance's (this new object created from the template) displayInfo();
+
+const school = {
+    name: "Hogwarts",
+    location: "Unknown",
+    established: "the 10th Century",
+    // b) To the ‘school’ object, add a function ‘displayInfo’
+    displayInfo() {
+        console.log(`My school ${this.name} was established in ${this.location} at ${this.established}`);
+}
+}
+//a) Console.log the school name.
+console.log(school.name);
+// c) Outside the class, Call the function ‘displayInfo’
+school.displayInfo();
+
+//////////////////////////////// Classes///////////////////////////////////////////////
+
+class CreateSchool{
+    constructor(name,location,established){
+        this.name = name;
+        this.location = location;
+        this.established = established;
+    }
+    displayInfo() {
+        console.log(`My school ${this.name} was established in ${this.location} at ${this.established}`);
+}
+}
+
+let createSchool = new CreateSchool("Generation", "Australia", 1000);
+createSchool.displayInfo();
